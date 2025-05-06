456691aad4bc234b8f8c4e56aecd8ca8522fd53f
Damiriko <damiriko80@gmail.com>
2025-05-06 22:49:46 +0300

    Первоначальная коммит

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+function App() {
+
+
+	return (
+		<>
+			<h1>Тест vite</h1>
+			<img src="/zastavka.jpg" alt="Заставка" />
+		</>
+	)
+}
+
+export default App
